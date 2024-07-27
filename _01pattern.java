f450b2e589dedbd6928f7de783eeea615128c003
sppatel1102 <webghost9360@gmail.com>
2024-07-27 17:42:33 +0530

    put in proper format
    update pattern in systemetic  way

--- _01pattern.java
+++ _01pattern.java
@@ -1,107 +1,136 @@
 import java.util.*;
 
 public class _01pattern {
-    public static void main(String[] args) {
-        // // pyrmid
-        // for (int i = 0; i < 5; i++) {
-        // for (int j = 0; j < i; j++) {
-        // System.out.print("*");
 
-        // }System.out.print("\n");
+    public static void pyramid() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
 
-        // }
+            }
+            System.out.print("\n");
 
-        // // reverse pyramid
-        // int n =8;
-        // for(int i = 1 ; i < n ;i++){
-        // for (int j = 1 ; j < n - i +1;j++){
-        // System.out.print("*");
-        // }System.out.print("\n");
-        // }
+        }
+    }
 
-        // // half pyramid
-        // int n = 5;
-        // for (int i = 0 ; i < n; i++){
-        // for (int j=1; j< i ;j++){
-        // System.out.print(j);
-        // }System.out.println("");
-        // }
+    public static void reverse_pyramid() {
+        int n = 8;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
 
-        // // Inverted & Rotated Half - Pyramid
-        // int n = 6;
-        // for (int i = 1; i <= n; i ++){
-        // for (int j = 1 ; j <= n - i ; j++){
-        // System.out.print(" ");
-        // }for (int j = 1; j <= i ; j++){
-        // System.out.print("*");
-        // }
-        // System.out.print("\n");
-        // }
+    }
 
+    public static void half_pyramid() {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void inverted_rotated_pyramid() {
+        int n = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void inverted_rorated_half_pyramid() {
         // // Inverted Half Pyramid with Number
-        // int n =5;
-        // for (int i = 1; i <=n ; i ++){
-        // for (int j = 1 ; j <= n-i+1 ; j++){
-        // System.out.print(j);
-        // }System.out.print("\n");
-        // }
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.print("\n");
+        }
+    }
 
-        // // Floyd' Traingle
-        // int n = 5;
-        // int a = 1;
-        // for (int i = 0 ; i < n ; i++){
-        // for (int j =0 ; j <= i ; j++){
-        // System.out.print(a + " ");
-        // a++;
-        // }
-        // System.out.println("");
-        // }
+    public static void flyod_tringle() {
+        // Floyd' Traingle
+        int n = 5;
+        int a = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(a + " ");
+                a++;
+            }
+            System.out.println("");
+        }
+    }
 
-        // // Butter fly Pattern
+    public static void butter_fly() {
+        // Butterfly Pattern
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
-        // int n = 5;
-        // for (int i = 1; i <= n; i++) {
-        // // stars
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // for (int j = 1; j <= 2 * (n - i); j++) {
-        // System.out.print(" ");
-        // }
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
+        for (int i = n; i >= 1; i--) {
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
-        // for (int i = n; i >= 1; i--) {
-        // // stars
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // for (int j = 1; j <= 2 * (n - i); j++) {
-        // System.out.print(" ");
-        // }
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // System.out.println();
-        // }
+    public static void rohumbus() {
+        // Rohumbus
 
-        // // Rohumbus
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
 
-        // int n = 10;
-        // for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
 
-        //     for (int j = 1; j <= (n - i); j++) {
-        //         System.out.print(" ");
-        //     }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
 
-        //     for (int j = 1; j <= n; j++) {
-        //         System.out.print("*");
+            }
+            System.out.println();
+        }
 
-        //     }
-        //     System.out.println();
-        // }
+    }
+
+    public static void main(String[] args) {
+
+        pyramid();
+        reverse_pyramid();
+        half_pyramid();
+        inverted_rotated_pyramid();
+        inverted_rorated_half_pyramid();
+        flyod_tringle();
+        butter_fly();
+        rohumbus();
     }
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+
+        reverse_pyramid();
+        half_pyramid();
+        inverted_rotated_pyramid();
+        inverted_rorated_half_pyramid();
+        flyod_tringle();
