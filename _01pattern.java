89f7186ebb8643e06470814861b78711b06c9831
sppatel1102 <webghost9360@gmail.com>
2024-08-01 20:48:49 +0530

    Types of Classes
    Opps concept

--- _01pattern.java
+++ _01pattern.java
@@ -1,4 +1,4 @@
-import java.util.*;
+
 
 public class _01pattern {
 
--- tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-
-        reverse_pyramid();
-        half_pyramid();
-        inverted_rotated_pyramid();
-        inverted_rorated_half_pyramid();
-        flyod_tringle();
